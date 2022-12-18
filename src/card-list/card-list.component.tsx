a580c9e6267fb2466f8f2f78175a6682d8cd83e7
Hossein Fasihi <hosein.fasihi.work@gmail.com>
2022-12-18 16:12:02 +0330

    updated the project with Typescript

--- src/card-list/card-list.component.tsx
+++ src/card-list/card-list.component.tsx
@@ -1,10 +1,15 @@
+import { Monster } from "../App";
+
 import "./card-list.styles.css";
 import Card from "../card/card.component";
 
+type CardListProps = {
+  monsters: Monster[];
+};
 
-const CardList = ({ monsters }) => (
+const CardList = ({ monsters }: CardListProps) => (
   <div className="card-list">
-    {monsters.map(monster => {
+    {monsters.map((monster) => {
       return <Card monster={monster} />;
     })}
   </div>
--- src/card/card.component.tsx
+++ src/card/card.component.tsx
@@ -1,7 +1,12 @@
-import "./card.styles.css";
-import React from "react";
+import { Monster } from "../App";
 
-const Card = ({ monster }) => {
+import "./card.styles.css";
+
+type CardProps = {
+  monster: Monster;
+};
+
+const Card = ({ monster }: CardProps) => {
   const { name, email, id } = monster;
   return (
     <div className="card-container">
